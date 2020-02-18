8b8994f35eeee8c5c9065ab7173aaabcb3705610
Makeespresso <makeespresso@gmail.com>
2020-02-17 19:56:38 -0700

    OurProducts Component Images Added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -10,17 +10,16 @@ export default class CarouselComp extends React.Component {
   }
 }
 
-
 // import React from 'react';
 // import Carousel from 'react-bootstrap/Carousel';
+// import Heroimages from '../assets/img/coverimage.jpeg';
 
-
-// function PicCarousel() {
+// function FunCarousel() {
 //   return (
 //     <div className="tester">
 //       < Carousel >
 //         <Carousel.Item>
-//           <img className="carouselImg" src="https://images.unsplash.com/photo-1579722820308-d74e571900a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" />
+//           <img className="carouselImg" src={Heroimages} alt="Omnivita" />
 //           <Carousel.Caption>
 
 //           </Carousel.Caption>
@@ -38,11 +37,8 @@ export default class CarouselComp extends React.Component {
 //           </Carousel.Caption>
 //         </Carousel.Item>
 //       </Carousel >
-//       <div className="banner">
-//         <p>Productos naturales de alta calidad para el cuidado de la salud.</p>
-//       </div>
 //     </div>
 //   )
 // }
 
-// export default PicCarousel;
+// export default FunCarousel;
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import InfoImg from '../assets/img/yoga.jpeg';
 
 export default function Info(props) {
   return (
     <div className="split">
       <div className="infoCenter center">
-        <img src="https://www.health.harvard.edu/media/content/images/p4_HeartMultivitamins_WL1811_gi918436996.jpg" alt="vitamins" />
+        <img src={InfoImg} alt="Info Image" />
       </div>
 
       <div className="infoCenter center">
-        <div className="center" style={{ backgroundColor: "peru" }}>
+        <div className="center" style={{ backgroundColor: "#E1C19D" }}>
           <h2>Tienda Naturista Omnivita</h2>
           <p>
             Dedicada a la venta al por menor de productos de la salud, nutrición y belleza. Incluyendo vitaminas, suplementos minerales, hierbas, nutrición deportiva, dietéticos, productos energéticos y de belleza. Nuestros productos son de alta calidad al alcance de la comunidad. Nos esmeramos por ofrecer productos legítimos al mejor precio para el cuidado de la salud y la belleza.
